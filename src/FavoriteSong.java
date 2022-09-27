3de45109289dd24bcd65fd4bfdfcc26f6ebe3936
Мамин Симпотяга <vov.85@mail.ru>
2022-09-27 14:39:53 +0300

    song

---
+++ src/FavoriteSong.java
@@ -0,0 +1,12 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Вот и осталось лишь снять усталость");
+        System.out.println("И этот вечер мне душу лечит.");
+        System.out.println("О-о о-о, зеленоглазое такси, о-о о-о, притормози, притормози");
+        System.out.println("О-о о-о, и отвези меня туда, о-о о-о, где будут рады мне всегда, всегда.");
+        System.out.println("Там и не спросят где меня носит");
+        System.out.println("Там, я-то знаю, все понимают.");
+        System.out.println("О-о о-о, зеленоглазое такси, о-о о-о, притормози, притормози");
+        System.out.println("О-о о-о, и отвези меня туда, о-о о-о, где будут рады мне всегда, всегда.");
+    }
+}
